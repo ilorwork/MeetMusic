1c777851ef02e12b8631db7340744fd185c98445
ilorwork <ilorwork64@gmail.com>
2022-09-20 00:04:55 +0300

    Install cookie parser

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 const { routesInit } = require("./routes/baseRouts");
 require("dotenv").config();
 require("./mongoConnect");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 // Allows the app to get a json format
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 app.use(express.static("client/build"));
 
